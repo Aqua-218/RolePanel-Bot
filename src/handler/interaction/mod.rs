7143924337bf946fc5a51f02ec0b9bfb88e831a3
Aqua-218 <aqua@arivell.com>
2026-01-09 01:44:45 +0900

    feat: add interaction handler module definition

---
+++ src/handler/interaction/mod.rs
@@ -0,0 +1,5 @@
+mod panel_edit;
+mod role_select;
+
+pub use panel_edit::{handle_panel_edit_interaction, handle_panel_modal_submit};
+pub use role_select::{handle_role_interaction, handle_role_select_with_values};
